63010efb6c8ffb2e3260cfc345afc4f9efc5a0cd
juamon22 <juan.montero@jjay.cuny.edu>
2026-03-03 23:57:45 -0500

    Add SecureUser class for user authentication

---
+++ SecureUser.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class User{
+    
+    private:
+    string username;
+    string password;
+    
+    public:
+    User(const string& uname, const string& pwd){
+        username = uname;
+        password = pwd;
+    }
+    bool login(const string& inputUser, const string& inputPwd) const {
+        return (inputUser == username) && (inputPwd == password);
+    }
+    void updatePassword( const string& newPwd) {
+        password = newPwd;
+    }
+};
