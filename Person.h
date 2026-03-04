85b406642782aa41f7213a73071a6be05186b349
juamon22 <juan.montero@jjay.cuny.edu>
2026-03-03 23:53:06 -0500

    Add Person class with introduction method

---
+++ Person.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+    public:
+    string name{"Maksi"};
+    int age{29};
+    char gender{'M'};
+    
+    public:
+    void introduce(){
+        cout<<"Hello, I'm "<< name<<". I'm "<<age<<" years old.";
+        if (gender == 'M') {
+            cout << " I'm a male."<<endl;
+        } else if (gender == 'F') {
+            cout<< " I'm a female."<<endl;
+        } else {
+            cout<<"Gender unspecified."<< endl;
+        }
+    }
+};
