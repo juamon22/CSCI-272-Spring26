997d70ebb66c9409a8300e1f19e7ec1d3d1376d8
juamon22 <juan.montero@jjay.cuny.edu>
2026-03-03 23:52:02 -0500

    Add BankAccount class with deposit and withdraw methods

---
+++ Lesson 4 BankAccount.h
@@ -0,0 +1,17 @@
+class BankAccount {
+    private:
+    double balance = 0.0;
+    
+    public:
+    void deposit(double amount){
+        balance += amount;
+    }
+    double getBalance() const {
+        return balance;
+    }
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        }
+    }
+};
